44da5794f194724cdef4f8ce8fc4e3c5c88b71b6
Cameron Hudson <camnn3@yahoo.com>
2020-12-22 16:44:08 +0000

    Automatically backed up by Learn

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -1,17 +1,17 @@
 class PetsController < ApplicationController
 
   get '/pets' do
-    @pets = Pet.all	    @pets = Pet.all
-    erb :'/pets/index' 	    erb :'/pets/index'
-  end	  end
+    @pets = Pet.all
+    erb :'/pets/index'
+  end
 
 
-  get '/pets/new' do 	  get '/pets/new' do
-    erb :'/pets/new'	    erb :'/pets/new'
-  end	  end
+  get '/pets/new' do 
+    erb :'/pets/new'
+  end
 
 
-  post '/pets' do 	  post '/pets' do
+  post '/pets' do
     @pet = Pet.create(params[:pet])
 
     if !params["owner"]["name"].empty?
@@ -21,17 +21,17 @@ class PetsController < ApplicationController
     @pet.save
 
 
-    redirect to "pets/#{@pet.id}"	    redirect to "pets/#{@pet.id}"
+    redirect to "pets/#{@pet.id}"
+  end
+
+
+  get '/pets/:id' do
+    @pet = Pet.find(params[:id])
+    erb :'/pets/show'
   end	  end
 
 
-  get '/pets/:id' do 	  get '/pets/:id' do
-    @pet = Pet.find(params[:id])	    @pet = Pet.find(params[:id])
-    erb :'/pets/show'	    erb :'/pets/show'
-  end	  end
-
-
-  patch '/pets/:id' do 	  get '/pets/:id/edit' do
+  patch '/pets/:id' do
     @pet = Pet.find(params[:id])
     erb :'/pets/edit'
   end

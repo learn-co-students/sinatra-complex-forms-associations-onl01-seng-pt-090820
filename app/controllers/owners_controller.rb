64048aae6de9964c098c80963c8e0e4a04f825bf
Cameron Hudson <camnn3@yahoo.com>
2020-12-22 16:47:12 +0000

    Automatically backed up by Learn

--- app/controllers/owners_controller.rb
+++ app/controllers/owners_controller.rb
@@ -1,46 +1,50 @@
 class OwnersController < ApplicationController
 
   get '/owners' do
-    @owners = Owner.all
-    erb :'/owners/index' 
-  end
+    @owners = Owner.all	    @owners = Owner.all
+    erb :'/owners/index' 	    erb :'/owners/index'
+  end	  end
 
-  get '/owners/new' do 
+
+  get '/owners/new' do 	  get '/owners/new' do
     @pets = Pet.all
-    erb :'/owners/new'
-  end
+    erb :'/owners/new'	    erb :'/owners/new'
+  end	  end
 
-  post '/owners' do 
-    @owner = Owner.create(params["owner"])
+
+  post '/owners' do
+
+    @owner = Owner.create(params[:owner])
     if !params["pet"]["name"].empty?
       @owner.pets << Pet.create(name: params["pet"]["name"])
     end
     redirect "/owners/#{@owner.id}"
-  end
+  end	  end
 
-  get '/owners/:id/edit' do 
-    @owner = Owner.find(params[:id])
-    erb :'/owners/edit'
-  end
 
-  get '/owners/:id' do 
-    @owner = Owner.find(params[:id])
-    erb :'/owners/show'
-  end
+  get '/owners/:id/edit' do
+    @owner = Owner.find(params[:id])	    @owner = Owner.find(params[:id])
+    @pets = Pet.all
+    erb :'/owners/edit'	    erb :'/owners/edit'
+  end	  end
 
-  patch '/owners/:id' do 
-    ####### bug fix
-    if !params[:owner].keys.include?("pet_ids")
-    params[:owner]["pet_ids"] = []
-    end
-    #######
- 
+
+  get '/owners/:id' do 	  get '/owners/:id' do
+    @owner = Owner.find(params[:id])	    @owner = Owner.find(params[:id])
+    erb :'/owners/show'	    erb :'/owners/show'
+  end	  end
+
+
+  patch '/owners/:id' do 	  patch '/owners/:id' do
+
     @owner = Owner.find(params[:id])
-    @owner.update(params["owner"])
+    @owner.update(params[:owner])
+
     if !params["pet"]["name"].empty?
       @owner.pets << Pet.create(name: params["pet"]["name"])
     end
-    redirect "owners/#{@owner.id}"
-  end
-end
+
+    redirect "/owners/#{@owner.id}"
+  end	  end
+end 	end
 
